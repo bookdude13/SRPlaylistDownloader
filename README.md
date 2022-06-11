82d8dbcaed3572c7e8074c389f494022d454c902
Ryan Fredlund <rfredlund13@gmail.com>
2022-06-11 16:20:06 -0500

    Initial commit, basic structure

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# SRPlaylistDownloader
-Automatically download all custom songs from Synthriderz playlists
+# Synth Riderz Playlist Downloader
+Automatically download all custom songs from Synth Riderz playlists when starting the game.
+
+
+## Installing
+1. Prepare Synth Riders for modding by following the [MelonLoader](https://melonwiki.xyz/#/README) wiki guide
+2. Download the newest version of SRPerformanceMeter from the Releases section. Your web browser may warn you about the .zip file because it contains a .dll - this is normal, and is how mods are built.
+3. Extract the contents of the .zip file to  `<path-to-synth-riders>\Mods` (create a new directory if it doesn't exist). On Windows, this will be at `C:\Program Files (x86)\Steam\steamapps\common\SynthRiders\Mods`
+4. Run Synth Riders and enjoy!
+
+
+## Disclaimer
+This mod is not affiliated with Synth Riders or Kluge Interactive. Use at your own risk.
+
---
+++ SRPlaylistDownloader/SRPlaylistDownloader/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SRPlaylistDownloader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SRPlaylistDownloader")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cce8f53b-8076-4fed-a900-a54eefdb5cdb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(SRPlaylistDownloader.MainMod), "SR Playlist Downloader", "1.0.0", "bookdude13", "https://github.com/bookdude13/SRPlaylistDownloader")]
+[assembly: MelonGame("Kluge Interactive", "SynthRiders")]
